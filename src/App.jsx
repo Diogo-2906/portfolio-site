58d76cae8a1f9a0cf1da3ae2da9b2200fe1fd866
Diogo2906 <diogo.ozeliero@aluno.senai.br>
2026-03-06 15:43:46 -0300

    animação fundo tela

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Trabalhos from "./components-react/Trabalhos";
 import Contatos from "./components-react/Contatos";
 import Footer from "./components-react/Footer";
 import Navbar from "./components-react/NavBar";
+import CurvedLoop from './Components-animations/CurvedLoop';
+import CurvedLoopFooter from "./Components-animations/CurvedLoopFooter";
 
 export default function App() {
   const [lightMode, setLightMode] = useState(false);
@@ -22,22 +24,37 @@ export default function App() {
 
   const toggleTheme = () => setLightMode((prev) => !prev);
 
+
+
   return (
     <div className="container">
-   <Navbar toggleTheme={toggleTheme} lightMode={lightMode} /> 
+      <Navbar toggleTheme={toggleTheme} lightMode={lightMode} />
 
       <section id="inicio">
         <Intro />
       </section>
+
+      <CurvedLoop marqueeText="Criatividade ✦ Modernidade ✦" />
+
       <section id="sobre">
         <Sobre />
       </section>
+
       <section id="educacao">
         <Educacao />
       </section>
+
       <section id="trabalhos">
         <Trabalhos />
       </section>
+
+      <CurvedLoopFooter
+        marqueeText="  Ideias ✦ em ✦ desenvolvimento ✦"
+        speed={1.5}
+        curveAmount={260}
+        interactive
+      />
+
       <section id="contatos">
         <Contatos />
       </section>
